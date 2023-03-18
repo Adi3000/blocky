4412f01e3522d2ace6090e1a3fd949e01fde8bd3
Adrien .John <adi300000@gmail.com>
2023-03-18 21:38:33 +0100

    Update server_endpoints.go

--- server/server_endpoints.go
+++ server/server_endpoints.go
@@ -223,15 +223,14 @@ func (s *Server) apiQuery(rw http.ResponseWriter, req *http.Request) {
 	useRemoteAdress := queryRequest.UseRemoteAddress
 	if useRemoteAdress {
 		remoteAddr, _, err := net.SplitHostPort(req.RemoteAddr)
+		apirw = APIResponseWriter{ip: remoteAddr}
 		if err != nil {
 			logAndResponseWithError(err, "Cannot find remote url on "+req.RemoteAddr+" : ", rw)
 
 			return
 		}
-		apirw = APIResponseWriter{ip: remoteAddr}
 	} else if queryRequest.RemoteAddress != "" {
 		apirw = APIResponseWriter{ip: queryRequest.RemoteAddress}
-
 	}
 
 	dnsRequest := util.NewMsgWithQuestion(query, qType)
